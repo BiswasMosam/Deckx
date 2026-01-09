8ea562930024915441efdeff6fd1844e1426999e
RY-1012 <115869604+RY-1012@users.noreply.github.com>
2026-01-09 11:13:46 +0530

    update

--- mobile_deckx/lib/main.dart
+++ mobile_deckx/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'screens/connection_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/home/home_screen.dart';
 
 void main() {
-  runApp(DeckXApp());
+  runApp(const DeckXApp());
 }
 
 class DeckXApp extends StatelessWidget {
+  const DeckXApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeckX',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.teal,
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'Roboto'),
-      ),
-      home: ConnectionScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
+      home: const HomeScreen(),
     );
   }
 }
